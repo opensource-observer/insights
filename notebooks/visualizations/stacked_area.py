e4036dc94c8977b9ac12824af52c3fa3d91f9972
ccerv1 <carl.cervone@gmail.com>
2023-10-15 08:19:33 -0400

    fix: parameters for stacked area charts

--- notebooks/visualizations/stacked_area.py
+++ notebooks/visualizations/stacked_area.py
@@ -3,30 +3,32 @@ import pandas as pd
 
 EVENT_TYPES = [
     'COMMIT_CODE',        
-     'PULL_REQUEST_CREATED',
-#     'PULL_REQUEST_MERGED',
-#     'PULL_REQUEST_CLOSED',
-     'PULL_REQUEST_APPROVED',
-     'ISSUE_CLOSED',
-     'ISSUE_CREATED'
+    'PULL_REQUEST_CREATED',
+    'PULL_REQUEST_MERGED',
+#    'PULL_REQUEST_CLOSED',
+    'PULL_REQUEST_APPROVED',
+    'ISSUE_CLOSED',
+    'ISSUE_CREATED'
 ]
 
 def activity_plot(
     dataframe,
     grouper_col,
     date_col='date',
-    value_col='num_contributions',
+    value_col=None,
     filter_col='event_type',
     filter_vals=EVENT_TYPES,
     ylabel='Total Contributions',
     start_date=None,
-    end_date=None
-    ):
+    end_date=None):
 
 
     fig, ax = plt.subplots(figsize=(10, 5), dpi=300)
 
-    dff = dataframe[dataframe[filter_col].isin(filter_vals)]
+    dff = dataframe[dataframe[filter_col].isin(filter_vals)].copy()
+    if value_col is None:
+        dff['value'] = 1
+        value_col = 'value'
     pivot_df = (dff
                 .groupby([date_col, grouper_col])[value_col]
                 .sum().reset_index()
