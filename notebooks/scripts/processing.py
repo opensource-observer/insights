fc6b5950505741d7a5d9712b8987aed606e1ae8c
ccerv1 <carl.cervone@gmail.com>
2023-10-14 23:03:39 -0400

    feat: active developers analysis

--- notebooks/scripts/processing.py
+++ notebooks/scripts/processing.py
@@ -31,3 +31,34 @@ def github_event_processor(result, forks=[]):
     df.reset_index(drop=True, inplace=True)
 
     return df
+
+
+def github_active_developers(dataframe, freq='month', min_ratio=0.3, event_filters=None, min_days=10):
+
+    denoms = {
+        'day': 1,
+        'week': 7,
+        'month': 30,
+        'quarter': 90,
+        'year': 365
+    }
+
+    if event_filters:
+        df = dataframe[dataframe['event_type'].isin(event_filters)]
+    else:
+        df = dataframe
+
+    pivot_table = df.pivot_table(index='contributor_name', columns=freq, values='date', aggfunc='nunique', fill_value=0)    
+    pivot_table = pivot_table[pivot_table.sum(axis=1) >= min_days]
+    pivot_table = pivot_table / denoms[freq]
+
+    devs = pd.DataFrame(index=['full-time', 'part-time', 'inactive'], columns=pivot_table.columns)
+    devs.loc['full-time'] = (pivot_table >= min_ratio).sum()
+    devs.loc['part-time'] = ((pivot_table < min_ratio) & (pivot_table > 0)).sum()    
+
+    for column in pivot_table.columns:
+        before_specified_column = pivot_table.loc[:, :column].sum(axis=1) > 0
+        current_devs = devs.loc[:,column].sum()
+        devs.loc['inactive', column] = before_specified_column.sum() - current_devs
+        
+    return devs
