24b261d00d239ae11021e0b01b83ff2d4b11917d
ccerv1 <carl.cervone@gmail.com>
2023-11-19 09:29:28 -0500

    feat: more options for venn diagrams

--- notebooks/visualizations/venn_diagram.py
+++ notebooks/visualizations/venn_diagram.py
@@ -3,12 +3,14 @@ from matplotlib_venn import venn3, venn3_circles
 
 
 DEFAULT_COLOR = "white"
+DEFAULT_FONT_COLOR = "black"
 
 def venn3_diagram(
     subsets, 
     labels,
     colors=[DEFAULT_COLOR,DEFAULT_COLOR,DEFAULT_COLOR],
     facecolor=DEFAULT_COLOR,
+    labelcolor=DEFAULT_FONT_COLOR,
     dpi=144,
     fontsize=8,
     figsize=10):
@@ -19,8 +21,13 @@ def venn3_diagram(
     
     plt.rcParams.update({'font.size': fontsize})
     fig, ax = plt.subplots(figsize=(figsize, figsize), dpi=dpi, facecolor=facecolor)        
-    venn3(subsets=subsets, set_labels=labels, set_colors=colors, alpha=1, ax=ax)
+    v = venn3(subsets=subsets, set_labels=labels, set_colors=colors, alpha=1, ax=ax)
     venn3_circles(subsets=subsets, linewidth=2)
     ax.set_facecolor(facecolor)
     
+    for text in v.set_labels:        
+        text.set_fontsize(fontsize+2)
+        text.set_weight('bold')
+        text.set_color(labelcolor)
+
     return fig, ax
