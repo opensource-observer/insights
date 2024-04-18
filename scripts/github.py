c61c7028787c18eb9d903edaffd3bf277b4cdd10
Carl Cervone <carl.cervone@gmail.com>
2024-04-18 09:10:03 -0400

    feat: github artifact validator

--- scripts/github.py
+++ scripts/github.py
@@ -2,14 +2,27 @@ from dotenv import load_dotenv
 import os
 import requests
 
+# Global variables initialized once
+load_dotenv()
+TOKEN = os.getenv("GITHUB_API_KEY")
+HEADERS = {"Authorization": f"Bearer {TOKEN}"}
+URL = "https://api.github.com/graphql"
+
+def send_graphql_request(query, variables):
+    response = requests.post(URL, json={"query": query, "variables": variables}, headers=HEADERS)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print("Error:", response.status_code)
+        print(response.text)
+        return None
+
+def extract_repo_owner_and_name(repo_path):
+    parts = repo_path.split('/')
+    return parts[0], (parts[1] if len(parts) > 1 else None)
 
 def is_fork(repo_path):
-
-    load_dotenv()
-    token = os.getenv("GITHUB_API_KEY")
-    owner, repo_name = repo_path.split('/')
-
-    url = "https://api.github.com/graphql"
+    owner, repo_name = extract_repo_owner_and_name(repo_path)
     query = """
         query IsRepoFork($owner: String!, $repoName: String!) {
             repository(owner: $owner, name: $repoName) {
@@ -17,67 +30,206 @@ def is_fork(repo_path):
             }
         }
     """
-    variables = {
-        "owner": owner,
-        "repoName": repo_name
-    }
-    headers = {
-        "Authorization": f"Bearer {token}"
-    }
-    response = requests.post(url, json={"query": query, "variables": variables}, headers=headers)
-
-    if response.status_code == 200:
-        data = response.json()
-        try:
-            is_fork = data["data"]["repository"]["isFork"]
-            return is_fork
-        except:
-            print("Error:", repo_path)
-            print(data)
-    else:
-        print("Error:", response.status_code)
-        print(response.text)
-
-
+    variables = {"owner": owner, "repoName": repo_name}
+    data = send_graphql_request(query, variables)
+    try:
+        return data["data"]["repository"]["isFork"]
+    except Exception as e:
+        print("Error parsing data:", e, data)
+        return None
 
 def get_license(repo_path):
-
-    load_dotenv()
-    token = os.getenv("GITHUB_API_KEY")
-    owner, repo_name = repo_path.split('/')
-
-    url = "https://api.github.com/graphql"
+    owner, repo_name = extract_repo_owner_and_name(repo_path)
     query = """
-        query getLicense($owner: String!, $repoName: String!) {
-        repository(owner: $owner, name: $repoName) {
-            licenseInfo {
-            spdxId,
-            url
+        query GetLicense($owner: String!, $repoName: String!) {
+            repository(owner: $owner, name: $repoName) {
+                licenseInfo {
+                    spdxId,
+                    url
+                }
             }
         }
+    """
+    variables = {"owner": owner, "repoName": repo_name}
+    data = send_graphql_request(query, variables)
+    try:
+        license_info = data["data"]["repository"]["licenseInfo"]
+        return license_info.get("spdxId") if license_info else None
+    except Exception as e:
+        print("Error parsing data:", e, data)
+        return None
+
+def get_owner_type(repo_path):
+    owner, _ = extract_repo_owner_and_name(repo_path)
+    query = """
+        query GetOwnerType($owner: String!) {
+            user(login: $owner) {
+                __typename
+            }
+            organization(login: $owner) {
+                __typename
+            }
         }
     """
-    variables = {
-        "owner": owner,
-        "repoName": repo_name
-    }
-    headers = {
-        "Authorization": f"Bearer {token}"
-    }
-    response = requests.post(url, json={"query": query, "variables": variables}, headers=headers)
+    variables = {"owner": owner}
+    data = send_graphql_request(query, variables)
+    try:
+        if data["data"]["user"]:
+            return "User"
+        elif data["data"]["organization"]:
+            return "Organization"
+        return None
+    except Exception as e:
+        print("Error parsing data:", e, data)
+        return None
+    
 
-    if response.status_code == 200:
-        data = response.json()
-        try:
-            license_info = data["data"]["repository"]["licenseInfo"]
-            if license_info:
-                license = license_info.get("spdxId", None)
-                return license
-            return None
-        except Exception as e:
-            print("Error:", repo_path)
-            print(e)
-            print(data)
+def get_repos(organization):
+    query = """
+        query GetRepositories($owner: String!) {
+            organization(login: $owner) {
+                repositories(first: 10, privacy: PUBLIC, isFork: false, isArchived: false) {
+                edges {
+                    node {
+                    name
+                    stargazerCount
+                    licenseInfo {
+                        name
+                    }
+                    }
+                }
+                }
+            }
+        }
+    """
+    variables = {"owner": organization}
+    data = send_graphql_request(query, variables)
+    try:
+        return data["data"]["organization"]["repositories"]["edges"]
+    except Exception as e:
+        print("Error parsing data:", e, data)
+        return None
+
+
+def get_commit_data(repo_path):
+    owner, repo_name = extract_repo_owner_and_name(repo_path)
+    query = """
+        query GetCommitData($owner: String!, $repoName: String!) {
+            repository(owner: $owner, name: $repoName) {
+                defaultBranchRef {
+                    target {
+                        ... on Commit {
+                            history(first: 100) {
+                                edges {
+                                    node {
+                                        author {
+                                            date
+                                            email
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    """
+    variables = {"owner": owner, "repoName": repo_name}
+    data = send_graphql_request(query, variables)
+    result = {
+        "commits": 0,
+        "authors": 0,
+        "unique_days": 0,
+        "newest_commit_date": None,
+        "oldest_commit_date": None
+    }
+    try:
+        edges = data["data"]["repository"]["defaultBranchRef"]["target"]["history"]["edges"]
+        emails = set()
+        dates = []
+        for node in edges:
+            author = node["node"]["author"]
+            if author:
+                result["commits"] += 1
+                emails.add(author["email"])
+                dates.append(author["date"].split("T")[0])
+        result["authors"] = len(emails)
+        result["unique_days"] = len(set(dates))
+        result["newest_commit_date"] = dates[0] if dates else None
+        result["oldest_commit_date"] = dates[-1] if dates else None
+        return result
+    except Exception as e:
+        print("Error parsing data:", e, data)
+        return None
+
+
+def validate_repo(repo):
+    commit_data = get_commit_data(repo)
+    if not commit_data:
+        return {
+            "Approved": False,
+            "Reason": "Repo no longer available."
+        }
+    if commit_data["commits"] < 10:
+        return {
+            "Approved": False,
+            "Reason": "Repositories must have at least 10 commits."
+        }
+    elif commit_data["commits"] < 100 and commit_data["oldest_commit_date"] > "2024-03-01":
+        print(commit_data)
+        return {
+            "Approved": False,
+            "Reason": "Repositories must have a commit older than 2024-03-01."
+        }
+    if commit_data["newest_commit_date"] < "2024-03-01":
+        return {
+            "Approved": False,
+            "Reason": "Repositories must have a commit newer than 2024-03-01."
+        }
+    if commit_data["unique_days"] < 5:
+        return {
+            "Approved": False,
+            "Reason": "Repositories must have commits on at least 5 different days."
+        }
+    return {
+        "Approved": True,
+        "Reason": None
+    }
+
+
+def validate_github_artifact(artifact):    
+    if "/" in artifact:
+        if is_fork(artifact):
+            return {
+                "Approved": False,
+                "Reason": "Repositories must not be forks."
+            }
+        return validate_repo(artifact)
+    elif get_owner_type(artifact) == "Organization":
+        repos = get_repos(artifact)
+        for repo_data in repos:
+            repo_name = repo_data["node"]["name"]
+            repo_path = f"{artifact}/{repo_name}"
+            result = validate_github_artifact(repo_path)
+            if result["Approved"]:
+                return result
+        return {
+            "Approved": False,
+            "Reason": f"No valid repositories found (n={len(repos)})."
+        }
     else:
-        print("Error:", response.status_code)
-        print(response.text)        
+        return {
+            "Approved": False,
+            "Reason": "No repo provided and owner is a User not an Organization."
+        }
+
+
+if __name__ == "__main__":
+    site = "opensource-observer"
+    print(validate_github_artifact(site))
+    #print(get_repos(site))
+    #print(get_owner_type(site))
+    #print(get_commit_data(site))
+    #print(is_fork(site))
+    #print(get_license(site))
