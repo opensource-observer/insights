35ee3dd2793b0f0aed5b9c4fafbf8a886db9d283
Carl Cervone <carl.cervone@gmail.com>
2024-07-15 22:21:50 -0400

    fix: audit script

--- analysis/optimism/retrofunding4/rf4_calcs_audit.py
+++ analysis/optimism/retrofunding4/rf4_calcs_audit.py
@@ -1,3 +1,4 @@
+import json
 import pandas as pd
 
 TOTAL_FUNDING = 10_000_000
@@ -5,57 +6,69 @@ MAX_CAP = 500_000
 MIN_CAP = 1000
 
 PRIVATE_DIR = "data/private"
-BALLOTS_CSV_PATH = f"{PRIVATE_DIR}/ballots final - export.csv"
+#BALLOTS_CSV_PATH = f"{PRIVATE_DIR}/ballots final - export.csv"
+BALLOTS_CSV_PATH = f"{PRIVATE_DIR}/EarlyBallotData.csv"
 DESTINATION_CSV_PATH = f"{PRIVATE_DIR}/rf4_results.csv"
 
 # create the Impact Metrics Share dataframe
 METRICS_CSV_PATH = "data/op_rf4_impact_metrics_by_project.csv"
 DF_METRICS = pd.read_csv(METRICS_CSV_PATH, index_col=1)
 METRIC_IDS = DF_METRICS.columns[-16:]
-DF_METRICS[METRIC_IDS] = DF_METRICS[METRIC_IDS].div(DF_METRICS[METRIC_IDS].sum(axis=0), axis=1)
+
+
+def parse_payload(json_payload):
+    ballot = json.loads(json_payload)
+    return {
+        'allocations': {
+            metric_id: weight
+            for metric_dict in ballot['allocations']
+            for metric_id, weight in metric_dict.items()
+        },
+        'os_multiplier': ballot['os_multiplier']
+    }
 
 
 def score_projects(ballot):
     
     # get the OS multiplier for each project
     os_multiplier = DF_METRICS['is_oss'].apply(lambda x: ballot['os_multiplier'] if x else 1)
-    metric_weights = ballot.drop('os_multiplier')
     
-    # score each project
-    scores = DF_METRICS[METRIC_IDS].multiply(metric_weights)
-    raw_project_scores = scores.sum(axis=1)
+    # apply the OS multiplier to each metric, normalize, and apply the ballot weights
+    scores = DF_METRICS[METRIC_IDS].copy()
+    for metric in METRIC_IDS:
+        scores[metric] = scores[metric] * os_multiplier
+        scores[metric] = scores[metric] / scores[metric].sum()
+        weight = ballot['allocations'].get(metric,0) / 100.
+        scores[metric] *= weight
+
+    # sum the score for each project
+    project_scores = scores.sum(axis=1)
     
-    # apply the OS multiplier to each project's total score
-    adjusted_project_scores = raw_project_scores * os_multiplier
-    
-    # return normalized scores
-    return adjusted_project_scores / adjusted_project_scores.sum(axis=0)
+    return project_scores
 
 
-def allocate_funding(project_scores, funding_available=TOTAL_FUNDING):
+def allocate_funding(project_scores, funding_balance=TOTAL_FUNDING):
 
     allocations = {}
-
-    # create a copy of projects so we can remove the ones above the cap
-    remaining_projects = project_scores.copy()
+    score_balance = project_scores.sum()
     
     # iterate through the projects in descending order of score
     for project_id, score in project_scores.sort_values(ascending=False).items():
         
-        # normalize and allocate the remaining funding
-        alloc = score / remaining_projects.sum() * funding_available
+        # determine the scaled, capped funding allocation for the project
+        uncapped_funding_alloc = score / score_balance * funding_balance
+        alloc = min(uncapped_funding_alloc, MAX_CAP)
 
-        # cap the allocation and adjust the funding available
-        if alloc >= MAX_CAP:
-            del remaining_projects[project_id]
-            allocations.update({project_id: MAX_CAP})
-            funding_available -= MAX_CAP
-        
-        # allocate the funding with no cap
-        else:
-            allocations.update({project_id: alloc})
+        # allocate funding to the project
+        allocations.update({project_id: alloc})
 
-    return pd.Series(allocations)
+        # update the funding and score balances
+        funding_balance -= alloc
+        score_balance -= score
+
+    project_allocations = pd.Series(allocations)
+
+    return project_allocations
 
 
 def main():
@@ -64,19 +77,12 @@ def main():
     df_ballots_raw = (
         pd.read_csv(BALLOTS_CSV_PATH)
         .query("Badgeholder == True & Status == 'SUBMITTED'")
-        .rename(columns={'Address': 'address', 'Os multiplier': 'os_multiplier'})
-        .set_index('address')
+        .set_index('Address')
     )
 
     # score the projects for each ballot
-    parse_ballot = lambda x: pd.Series({k: v for d in eval(x) for k, v in d.items()})
-    df_ballots = (
-        pd.concat([
-            df_ballots_raw['Ballot content'].apply(parse_ballot).fillna(0) / 100,
-            df_ballots_raw['os_multiplier']
-        ], axis=1)
-    )
-    df_scores = df_ballots.apply(score_projects, axis=1)
+    df_ballots = df_ballots_raw['Payload'].apply(parse_payload)
+    df_scores = df_ballots.apply(score_projects)
 
     # allocate funding for each badgeholder
     df_results = df_scores.apply(allocate_funding, axis=1).T
@@ -101,6 +107,7 @@ def main():
     df_consolidated = DF_METRICS.join(df_results).sort_values(by='rf4_allocation', ascending=False)
     df_consolidated['rf4_allocation'].fillna(0)
     df_consolidated.to_csv(DESTINATION_CSV_PATH)
+
 
 if __name__ == "__main__":
     main()
