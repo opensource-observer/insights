7c3e55a98ce0f81006c7047c925d1628b57ecc84
Carl Cervone <carl.cervone@gmail.com>
2024-09-23 05:47:01 -0400

    feat(nb): new projects & creator metrics

--- community/sunny_metrics/queries/nft.py
+++ community/sunny_metrics/queries/nft.py
@@ -1,18 +1,28 @@
 from config import DATE_START, DATE_END, X, Y
 
-def artnft_ndrops(blockchain: str, contract_address: str) -> str:
+
+def creator_contracts(blockchain: str, wallet_address: str) -> str:
     return f"""
-    WITH nft_contracts AS (
-        SELECT DISTINCT nft_address
-        FROM {blockchain}.nft.ez_nft_transfers
-        WHERE block_timestamp BETWEEN '{DATE_START}' AND '{DATE_END}'
-    )
-    SELECT 
-        COUNT(*) AS number_of_drops
-    FROM nft_contracts nc
-    JOIN {blockchain}.core.dim_contracts dc ON nc.nft_address = dc.address
-    WHERE dc.created_block_timestamp BETWEEN '{DATE_START}' AND '{DATE_END}';
+    SELECT address AS nft_address
+    FROM {blockchain}.core.dim_contracts
+    WHERE creator_address = '{wallet_address}'
+    AND created_block_timestamp BETWEEN '{DATE_START}' AND '{DATE_END}';
     """
+
+
+# def artnft_ndrops(blockchain: str, contract_address: str) -> str:
+#     return f"""
+#     WITH nft_contracts AS (
+#         SELECT DISTINCT nft_address
+#         FROM {blockchain}.nft.ez_nft_transfers
+#         WHERE block_timestamp BETWEEN '{DATE_START}' AND '{DATE_END}'
+#     )
+#     SELECT 
+#         COUNT(*) AS number_of_drops
+#     FROM nft_contracts nc
+#     JOIN {blockchain}.core.dim_contracts dc ON nc.nft_address = dc.address
+#     WHERE dc.created_block_timestamp BETWEEN '{DATE_START}' AND '{DATE_END}';
+#     """
 
 def artnft_nam(blockchain: str, wallet_address: str) -> str:
     return f"""
