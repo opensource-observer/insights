aa14637e093695e934beb003df01dccaabf8c16b
Evan Meyer <evanm6@illinois.edu>
2024-12-23 21:54:08 -0600

    feat: finished streamlit dashboards for subset of s6 grants (minus defillama data)

--- analysis/optimism/govfund_grants/metrics/single_project_visualizations.py
+++ analysis/optimism/govfund_grants/metrics/single_project_visualizations.py
@@ -2,18 +2,19 @@ from datetime import datetime, timedelta
 from google.cloud import bigquery
 import matplotlib.pyplot as plt
 import pandas as pd
-import os
+from typing import List, Dict, Tuple, Union, Optional
 
-PROJECT_START_DATE = '2024-08-31'
+PROJECT_START_DATE = '2024-09-01'
+PROJECT_START_DATE_DT = datetime(2024, 9, 1)
 PROJECT_NETWORK = 'mainnet'
 NUMERIC_COLS = ['transaction_cnt', 'active_users', 'unique_users', 'total_op_transferred']
 BIGQUERY_PROJECT_NAME = 'oso-data-436717'
 
-
-def generate_dates():
+# create a list of all dates from now to the start date
+def generate_dates() -> List[str]:
     dates = []
     todays_date = datetime.now()
-    target_date = datetime(2024, 9, 1)
+    target_date = PROJECT_START_DATE_DT
     date_interval = (todays_date - target_date).days
     for i in range(date_interval):
         date = target_date + timedelta(days=i)
@@ -21,8 +22,8 @@ def generate_dates():
 
     return dates
 
-
-def extract_addresses(project_dict):
+# get all of the addresses relevant to the target project
+def extract_addresses(project_dict: Dict[str, List[Dict[str, Dict[str, Union[str, int]]]]]) -> Tuple[str, ...]:
     project_address_dict = project_dict['addresses']
 
     project_addresses = []
@@ -31,22 +32,22 @@ def extract_addresses(project_dict):
     
     return tuple(project_addresses)
 
-
-def make_dates_df(dates, project_addresses):
+# create a dataframe with a row for each date and address combination
+def make_dates_df(dates: List[str], project_addresses: Tuple[str, ...]) -> pd.DataFrame:
     data = []
     for address in project_addresses:
         for date in dates:
-            data.append({'transaction_date': date, 'contract_address': address})
+            data.append({'transaction_date': date, 'address': address})
 
     return pd.DataFrame(data)
 
-
-def query_daily_transactions(client, project_addresses, dates_df):
+# query the daily transaction data of the project addresses over the interval and store it in a dataframe
+def query_daily_transactions(client: bigquery.Client, project_addresses: Tuple[str, ...], dates_df: pd.DataFrame) -> pd.DataFrame:
     try:
         daily_transactions_query = f"""
             SELECT 
                 dt AS transaction_date,
-                to_address AS contract_address,
+                to_address AS address,
                 COUNT(*) AS transaction_cnt,
                 COUNT(from_address) AS active_users,
                 COUNT(DISTINCT from_address) AS unique_users,
@@ -61,12 +62,14 @@ def query_daily_transactions(client, project_addresses, dates_df):
         daily_transactions_result = client.query(daily_transactions_query)
         daily_transactions_df = daily_transactions_result.to_dataframe()
 
-        daily_transactions_merged_df = pd.merge(daily_transactions_df, dates_df, how='outer', on=['transaction_date', 'contract_address']).fillna(0) 
+        # merge the queried data with the dates dataframe to ensure we have 0 rows for days without transactions
+        daily_transactions_merged_df = pd.merge(daily_transactions_df, dates_df, how='outer', on=['transaction_date', 'address']).fillna(0) 
         
+        # ensure datatypes align of the columns we'll be working with later
         daily_transactions_merged_df[NUMERIC_COLS] = daily_transactions_merged_df[NUMERIC_COLS].astype(int)
 
         daily_transactions_merged_df = (daily_transactions_merged_df
-            .groupby(['transaction_date', 'contract_address'], as_index=False)
+            .groupby(['transaction_date', 'address'], as_index=False)
             .agg({
                 'transaction_cnt': 'sum',
                 'active_users': 'sum',
@@ -80,8 +83,8 @@ def query_daily_transactions(client, project_addresses, dates_df):
     except Exception as e:
         raise RuntimeError(f"Failed to query daily transactions: {e}")
 
-
-def query_op_flow(client, project_addresses):
+# create a table of all transactions (in and out) involving one of the project addresses over the interval
+def query_op_flow(client: bigquery.Client, project_addresses: Tuple[str, ...]) -> pd.DataFrame:
     try:
         op_flow_query = f"""
         (SELECT 
@@ -120,16 +123,14 @@ def query_op_flow(client, project_addresses):
     except Exception as e:
         raise RuntimeError(f"Failed to query op flow: {e}")
 
-
-def plot_top_addresses(op_flow, project_title):
-    # Create a bar chart for top addresses by transaction count
+# create a bar chart for top addresses by transaction count
+def plot_top_n_addresses(op_flow: pd.DataFrame, project_title: str, n: int) -> plt.Figure:
     address_counts = op_flow.groupby('from_address')['cnt'].sum().sort_values(ascending=False)
 
     fig, ax = plt.subplots(figsize=(12, 6))
-    address_counts.head(10).plot(kind='barh', ax=ax, color='skyblue')
+    address_counts.head(n).plot(kind='barh', ax=ax, color='skyblue')
 
-    # Customize plot
-    ax.set_title(f'Top 10 From Addresses by Transaction Count ({project_title})', fontsize=14)
+    ax.set_title(f'Top {n} From Addresses by Transaction Count ({project_title})', fontsize=14)
     ax.set_xlabel('Transaction Count')
     ax.set_ylabel('From Address')
     ax.grid(axis='x', linestyle='--', alpha=0.7)
@@ -137,50 +138,60 @@ def plot_top_addresses(op_flow, project_title):
 
     return fig
 
-
-def plot_net_op_flow(op_flow_df, project_addresses):
-    def transaction_direction(row):
+# create a dataset that represents net transactions by factoring in transaction direction
+def make_net_op_dataset(op_flow_df: pd.DataFrame, project_addresses: Tuple[str, ...]) -> pd.DataFrame:
+    # helper to determine transaction direction
+    def transaction_direction(row: pd.Series) -> str:
         if row['from_address'] in project_addresses:
             return "out"
         elif row['to_address'] in project_addresses:
             return "in"
         return ""
 
-    # Apply direction logic
+    # apply direction logic
     op_flow_df['direction'] = op_flow_df.apply(transaction_direction, axis=1)
 
-    # Prepare cumulative transaction data
+    # prepare cumulative transaction data
     transaction_direction_df = pd.concat([
-        op_flow_df[['transaction_date', 'from_address', 'direction', 'cnt', 'total_op_transferred']]
-        .rename(columns={'from_address': 'wallet_address'}),
-        op_flow_df[['transaction_date', 'to_address', 'direction', 'cnt', 'total_op_transferred']]
-        .rename(columns={'to_address': 'wallet_address'})
+        op_flow_df[['transaction_date', 'from_address', 'direction', 'cnt', 'total_op_transferred', 'total_op_transferred_in_tokens']]
+        .rename(columns={'from_address': 'address'}),
+        op_flow_df[['transaction_date', 'to_address', 'direction', 'cnt', 'total_op_transferred', 'total_op_transferred_in_tokens']]
+        .rename(columns={'to_address': 'address'})
     ])
 
-    # Aggregate and calculate cumulative sum
+    # aggregate and calculate cumulative sum
     transaction_direction_df.drop_duplicates(inplace=True)
-    transaction_direction_df = transaction_direction_df.groupby(['transaction_date', 'wallet_address', 'direction'], as_index=False).agg({
+    transaction_direction_df = transaction_direction_df.groupby(['transaction_date', 'address', 'direction'], as_index=False).agg({
         'cnt': 'sum',
-        'total_op_transferred': 'sum'
+        'total_op_transferred': 'sum',
+        'total_op_transferred_in_tokens': 'sum'
     })
 
     transaction_direction_df.loc[transaction_direction_df['direction'] == 'out', 'total_op_transferred'] *= -1
-    transaction_direction_df.sort_values(by=['wallet_address', 'transaction_date'], inplace=True)
-    transaction_direction_df['cum_op_transferred'] = transaction_direction_df.groupby('wallet_address')['total_op_transferred'].cumsum()
+    transaction_direction_df.loc[transaction_direction_df['direction'] == 'out', 'total_op_transferred_in_tokens'] *= -1
 
-    # Pivot for plotting
+    transaction_direction_df.sort_values(by=['address', 'transaction_date'], inplace=True)
+    transaction_direction_df['cum_op_transferred'] = transaction_direction_df.groupby('address')['total_op_transferred'].cumsum()
+    transaction_direction_df['cum_op_transferred_in_tokens'] = transaction_direction_df.groupby('address')['total_op_transferred_in_tokens'].cumsum()
+
+    return transaction_direction_df
+
+# plot the net op flow dataset as a line graph
+def plot_net_op_flow(op_flow_df: pd.DataFrame, project_addresses: Tuple[str, ...]) -> plt.Figure:
+    transaction_direction_df = make_net_op_dataset(op_flow_df, project_addresses)
+
+    # pivot for plotting
     transaction_direction_pivoted_df = transaction_direction_df.pivot(
         index='transaction_date',
-        columns='wallet_address',
+        columns='address',
         values='cum_op_transferred'
     ).fillna(0)
 
-    # Create the plot
+    # create the plot
     fig, ax = plt.subplots(figsize=(12, 6))
     for wallet_address in project_addresses:
         ax.plot(transaction_direction_pivoted_df.index, transaction_direction_pivoted_df[wallet_address], label=wallet_address)
 
-    # Customize plot
     ax.set_title('Cumulative OP Transferred Over Time by Wallet Address', fontsize=14)
     ax.set_xlabel('Transaction Date')
     ax.set_ylabel('Cumulative OP Transferred')
@@ -191,12 +202,12 @@ def plot_net_op_flow(op_flow_df, project_addresses):
 
     return fig
 
-
-def _plot_single_metric(daily_transactions, metric, metric_title, project_title):
+# create a line graph of the dataset provided for the chosen metric
+def _plot_single_metric(daily_transactions: pd.DataFrame, metric: str, metric_title: str, project_title: str) -> plt.Figure:
     daily_transactions['transaction_date'] = pd.to_datetime(daily_transactions['transaction_date'])
 
-    data_grouped = daily_transactions.groupby(['transaction_date', 'contract_address'])[metric].sum().reset_index()
-    pivoted_data = data_grouped.pivot(index='transaction_date', columns='contract_address', values=metric).fillna(0)
+    data_grouped = daily_transactions.groupby(['transaction_date', 'address'])[metric].sum().reset_index()
+    pivoted_data = data_grouped.pivot(index='transaction_date', columns='address', values=metric).fillna(0)
 
     fig, ax = plt.subplots(figsize=(12, 6))
 
@@ -245,13 +256,15 @@ def plot_total_op_transferred(daily_transactions, project_title):
         project_title=project_title
     )
 
+# generate and return all of the graphs
+def generate_visualizations(project_addresses: Tuple[str, ...], daily_transactions: pd.DataFrame, op_flow: pd.DataFrame, project_title: str, n: int = 10) -> Tuple[
+    plt.Figure, plt.Figure, plt.Figure, plt.Figure, plt.Figure, Optional[plt.Figure]]:
 
-def generate_visualizations(project_addresses, daily_transactions, op_flow, project_title):
     transaction_count_plot = plot_transaction_count(daily_transactions, project_title)
     active_users_plot = plot_active_users(daily_transactions, project_title)
     unique_users_plot = plot_unique_users(daily_transactions, project_title)
     total_op_transferred_plot = plot_total_op_transferred(daily_transactions, project_title)
-    top_addresses_plot = plot_top_addresses(op_flow, project_title)
+    top_addresses_plot = plot_top_n_addresses(op_flow, project_title, n)
 
     net_op_flow_plot = None
     if len(project_addresses) < 12:
@@ -259,11 +272,16 @@ def generate_visualizations(project_addresses, daily_transactions, op_flow, proj
 
     return transaction_count_plot, active_users_plot, unique_users_plot, total_op_transferred_plot, top_addresses_plot, net_op_flow_plot
 
-
-def process_project(client, project_addresses, dates):
+# given a bigquery client, the addresses of a target project, and a blank list of dates, return all of the desired dataframes
+def process_project(client: bigquery.Client, project_addresses: Tuple[str, ...], dates: List[str]) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     dates_df = make_dates_df(dates, project_addresses)
 
     daily_transactions = query_daily_transactions(client, project_addresses, dates_df)
+    daily_transactions['total_op_transferred_in_tokens'] = daily_transactions['total_op_transferred'] / 1e15
+    
     op_flow = query_op_flow(client, project_addresses)
+    op_flow['total_op_transferred_in_tokens'] = op_flow['total_op_transferred'] / 1e15
+    
+    net_op_flow = make_net_op_dataset(op_flow, project_addresses)
 
-    return daily_transactions, op_flow
+    return daily_transactions, op_flow, net_op_flow
